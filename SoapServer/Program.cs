403aa7b48cae36a1f214812bee90817315c2ea61
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2025-06-10 10:35:02 +0100

    fix soap service from official template

--- SoapServer/Program.cs
+++ SoapServer/Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<Service>();
+    serviceBuilder.AddServiceEndpoint<Service, IService>(
+        new BasicHttpBinding(BasicHttpSecurityMode.Transport), 
+        "/Service.svc"
+    );
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpsGetEnabled = true;
+});
 
 app.Run();
